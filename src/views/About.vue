0381dc557f306d7d4e92ae3fdcc5903b7f8aebd1
moa1520 <douit_be@naver.com>
2019-10-12 23:19:35 +0900

    Until 7

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <v-alert type="primary" :value="true" v-for="i in array">
+      반복중 {{ i }}
+    </v-alert>
   </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      array: [1, 2, 3, 4, 'www', 'xxx']
+    }
+  }
+}
+</script>
